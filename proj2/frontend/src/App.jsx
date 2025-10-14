1b1a4a17aeac89e024fd25146fdb369f56c03b9a
Cynthia Espinoza-Arredondo <c_espinozaa@uncg.edu>
2025-10-14 14:49:22 -0400

    initial set up

--- proj2/frontend/src/App.jsx
+++ proj2/frontend/src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import ItemList from './components/ItemList'
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    
-      FastAPI + MongoDB + React
-      
-    
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+        </a>
+        <a href="https://react.dev" target="_blank">
+
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
